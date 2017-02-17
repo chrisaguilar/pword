5c20547f2c6dcd69ad8e1b7a9b49774c096b5e5c
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-16 21:39:16 -0500

    This is what I want

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+require('ts-node').register({
+  cacheDirectory: require('path').resolve(__dirname, '.tsnode_cache/'),
+  disableWarnings: true,
+  fast: true,
+  ignore: false,
+  lazy: true
+});
+require('./src/cli');
