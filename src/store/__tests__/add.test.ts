0af8ee9f8e97b22f64b0f409b11f0b4eb7b05994
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-17 01:25:08 -0500

    Missed a try/catch

--- src/store/__tests__/add.test.ts
+++ src/store/__tests__/add.test.ts
@@ -7,29 +7,41 @@ describe('store -> add', () => {
   afterAll(async () => await destroy());
 
   test('saves new password to store', async () => {
-    const store = await open();
+    try {
 
-    store.set('gmail', 'abc123');
-    await close(store);
-    expect(await open()).toEqual(store);
+      const store = await open();
 
-    const added = await add('facebook', '123abc');
-    store.set('facebook', '123abc');
-    expect(await open()).toEqual(store);
+      store.set('gmail', 'abc123');
+      await close(store);
+      expect(await open()).toEqual(store);
 
-    expect(added).toEqual(`${b}${green}facebook${r} set to ${b}${green}123abc${r}`);
+      const added = await add('facebook', '123abc');
+      store.set('facebook', '123abc');
+      expect(await open()).toEqual(store);
+
+      expect(added).toEqual(`${b}${green}facebook${r} set to ${b}${green}123abc${r}`);
+
+    } catch (e) {
+      throw e;
+    }
   });
 
   test('rejects names already present', async () => {
-    const store = await open();
+    try {
 
-    store.set('gmail', 'abc123');
-    await close(store);
-    expect(await open()).toEqual(store);
+      const store = await open();
 
-    const rejected = await add('gmail', '123abc');
-    store.set('gmail', '123abc');
-    expect(await open()).not.toEqual(store);
-    expect(rejected).toEqual(`${b}${red}gmail already present in store${r}`);
+      store.set('gmail', 'abc123');
+      await close(store);
+      expect(await open()).toEqual(store);
+
+      const rejected = await add('gmail', '123abc');
+      store.set('gmail', '123abc');
+      expect(await open()).not.toEqual(store);
+      expect(rejected).toEqual(`${b}${red}gmail already present in store${r}`);
+
+    } catch (e) {
+      throw e;
+    }
   });
 });
