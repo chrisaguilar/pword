13c019cf80dc950b05284c7bafa6066299e00cab
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-16 21:40:43 -0500

    Removed i, x, renamed s to a i and x can be implemented later, I'm kind of in a hurry

--- src/cli/help.ts
+++ src/cli/help.ts
@@ -6,14 +6,16 @@ export const help = function help (): string {
 
   -h, --help                          Print this screen
   -v, --version                       Print the version number
+  -a, --add <name> <password>         Save a new password
   -d, --delete <name>                 Delete a password
   -e, --edit <name> <new_password>    Give <name> a new password
   -f, --find <RegEx>                  Find passwords
   -g, --get <name>                    Get a password
-  -i, --import <keyfile> <store>      Import a previously created database
   -n, --new [length] <name>           Generate a new password of [length] characters (Default: 20)
   -r, --rename <from> <to>            Rename a password
-  -s, --save <name> <password>        Save a new password
-  -x, --export [p|j|e] <destination>  Export key/store in (p)laintext, (j)son, or (e)ncrypted form (Default: e)
 `;
 };
+
+// FUTURE
+// -i, --import <keyfile> <store>      Import a previously created database
+// -x, --export [p|j|e] <destination>  Export key/store in (p)laintext, (j)son, or (e)ncrypted form (Default: e);
