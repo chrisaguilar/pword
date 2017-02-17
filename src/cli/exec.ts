fc8890503ba73f32332d499fd141882ed28f8104
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-16 21:40:13 -0500

    This is, essentially, finished. I just need to go back to the store and make it work.

---
+++ src/cli/exec.ts
@@ -0,0 +1,23 @@
+import { add, del, edit, find, get, newpass, rename } from '../store';
+import { help } from './help';
+import { parse } from './parse';
+
+const { version } = require('../../package.json');
+
+export const exec = async function exec() {
+  const [ opt, ...args ] = parse(process.argv.slice(2).join(' '));
+  return console.log(
+    opt === 'a' ? await add(args[0], args[1])             :
+    opt === 'd' ? await del(args[0])                      :
+    opt === 'e' ? await edit(args[0], args[1])            :
+    opt === 'f' ? await find(args[0])                     :
+    opt === 'g' ? await get(args[0])                      :
+    opt === 'h' ? help()                                  :
+    opt === 'i' ? 'Importing Not Implemented'             :
+    opt === 'n' ? await newpass(+args[0] || 20, args[1])  :
+    opt === 'r' ? await rename(args[0], args[1])          :
+    opt === 'v' ? version                                 :
+    opt === 'x' ? 'Exporting Not Implemented'
+                : help()
+  );
+};
