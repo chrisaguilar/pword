49f26892cc6b31f73338f4a7f37ab0ad29095608
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-16 21:42:25 -0500

    Basically the final form of `add`

--- src/store/__tests__/add.test.ts
+++ src/store/__tests__/add.test.ts
@@ -1,5 +1,7 @@
 import { destroy, initialize } from '.';
 import { add, close, open } from '..';
+import { formatters } from '../../lib';
+const { b, green, r, red } = formatters;
 
 describe('store -> add', () => {
   beforeAll(async () => await initialize());
@@ -12,8 +14,23 @@ describe('store -> add', () => {
     await close(store);
     expect(await open()).toEqual(store);
 
-    await add('facebook', '123abc');
+    const added = await add('facebook', '123abc');
     store.set('facebook', '123abc');
     expect(await open()).toEqual(store);
+
+    expect(added).toEqual(`${b}${green}facebook${r} set to ${b}${green}123abc${r}`);
+  });
+
+  test('rejects names already present', async () => {
+    const store = await open();
+
+    store.set('gmail', 'abc123');
+    await close(store);
+    expect(await open()).toEqual(store);
+
+    const rejected = await add('gmail', '123abc');
+    store.set('gmail', '123abc');
+    expect(await open()).not.toEqual(store);
+    expect(rejected).toEqual(`${b}${red}gmail already present in store${r}`);
   });
 });
--- src/store/add.ts
+++ src/store/add.ts
@@ -1,10 +1,20 @@
 import { close, open } from '.';
+import { formatters } from '../lib';
+const { b, green, r, red } = formatters;
 
-export const add = async function add(name: string, password: string): Promise<void> {
+export const add = async function add(name: string, password: string): Promise<string> {
   try {
+
     const store = await open();
+
+    if (store.get(name))
+      return `${b}${red}${name} already present in store${r}`;
+
     store.set(name, password);
-    return await close(store);
+    await close(store);
+
+    return `${b}${green}${name}${r} set to ${b}${green}${password}${r}`;
+
   } catch (e) {
     throw e;
   }
