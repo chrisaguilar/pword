22ed105f01838a51a902ee177081c3b0e5ca7b50
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-16 22:12:57 -0500

    Catches exceptions, now

--- src/cli/exec.ts
+++ src/cli/exec.ts
@@ -4,20 +4,26 @@ import { parse } from './parse';
 
 const { version } = require('../../package.json');
 
-export const exec = async function exec() {
-  const [ opt, ...args ] = parse(process.argv.slice(2).join(' '));
-  return console.log(`\n${
-    opt === 'a' ? await add(args[0], args[1])             :
-    opt === 'd' ? await del(args[0])                      :
-    opt === 'e' ? await edit(args[0], args[1])            :
-    opt === 'f' ? await find(args[0])                     :
-    opt === 'g' ? await get(args[0])                      :
-    opt === 'h' ? help()                                  :
-    opt === 'i' ? 'Importing Not Implemented'             :
-    opt === 'n' ? await newpass(+args[0] || 20, args[1])  :
-    opt === 'r' ? await rename(args[0], args[1])          :
-    opt === 'v' ? version                                 :
-    opt === 'x' ? 'Exporting Not Implemented'
-                : help()
-  }\n`);
+export const exec = async function exec(): Promise<void> {
+  try {
+
+    const [ opt, ...args ] = parse(process.argv.slice(2).join(' '));
+    return console.log(`\n${
+      opt === 'a' ? await add(args[0], args[1])             :
+      opt === 'd' ? await del(args[0])                      :
+      opt === 'e' ? await edit(args[0], args[1])            :
+      opt === 'f' ? await find(args[0])                     :
+      opt === 'g' ? await get(args[0])                      :
+      opt === 'h' ? help()                                  :
+      opt === 'i' ? 'Importing Not Implemented'             :
+      opt === 'n' ? await newpass(+args[0] || 20, args[1])  :
+      opt === 'r' ? await rename(args[0], args[1])          :
+      opt === 'v' ? version                                 :
+      opt === 'x' ? 'Exporting Not Implemented'
+                  : help()
+    }\n`);
+
+  } catch (e) {
+    throw e;
+  }
 };
