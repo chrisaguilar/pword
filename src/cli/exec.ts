026a183d8b2f07a4c52b78cd973b4efb1e6dfcfa
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-17 01:25:41 -0500

    Got rid of newlines, needed to properly handle `find`

--- src/cli/exec.ts
+++ src/cli/exec.ts
@@ -4,15 +4,21 @@ import { parse } from './parse';
 
 const { version } = require('../../package.json');
 
+const decide = function decide (thing: string|string[]): string {
+  if (typeof thing === 'string') return thing;
+  for (const i of thing) console.log(i);
+  return '';
+};
+
 export const exec = async function exec(): Promise<void> {
   try {
 
     const [ opt, ...args ] = parse(process.argv.slice(2).join(' '));
-    return console.log(`\n${
+    return console.log(`${
       opt === 'a' ? await add(args[0], args[1])             :
       opt === 'd' ? await del(args[0])                      :
       opt === 'e' ? await edit(args[0], args[1])            :
-      opt === 'f' ? await find(args[0])                     :
+      opt === 'f' ? decide(await find(args[0]))             :
       opt === 'g' ? await get(args[0])                      :
       opt === 'h' ? help()                                  :
       opt === 'i' ? 'Importing Not Implemented'             :
@@ -21,7 +27,7 @@ export const exec = async function exec(): Promise<void> {
       opt === 'v' ? version                                 :
       opt === 'x' ? 'Exporting Not Implemented'
                   : help()
-    }\n`);
+    }`);
 
   } catch (e) {
     throw e;
