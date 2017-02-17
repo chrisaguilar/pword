5e20408054c9a375b3eb8e5d04d2bd45ce3800a0
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-16 22:01:25 -0500

    Finished

--- src/store/__tests__/edit.test.ts
+++ src/store/__tests__/edit.test.ts
@@ -1,21 +1,40 @@
 import { destroy, initialize } from '.';
 import { close, edit, open } from '..';
+import { b, green, r, red } from '../../lib/formatters';
+
 
 describe('store -> edit', () => {
   beforeAll(async () => await initialize());
   afterAll(async () => await destroy());
 
-  test('finds passwords that match a given RegEx', async () => {
-      let store = await open();
+  test('gives items new passwords', async () => {
+    try {
+
+      const store = await open();
       store.set('Outlook MaIl', 'abc123');
       store.set('gmail', 'gmailPass');
       store.set('facebook', 'notmymailpass');
       await close(store);
 
-      await edit('facebook', 'newpass');
+      const edited = await edit('facebook', 'newpass');
       store.set('facebook', 'newpass');
 
       expect(await open()).toEqual(store);
+      expect(edited).toEqual(`${b}${green}facebook${r} set to ${b}${green}newpass${r}`);
+
+    } catch (e) {
+      throw e;
     }
-  );
+  });
+
+  test('alerts user if the item does not exist in the store', async () => {
+    try {
+
+      const rejected = edit('whatwhat', 'bzxcnbvxnvc');
+      expect(rejected).toEqual(`${b}${red}whatwhat not in the store${r}`);
+
+    } catch (e) {
+      throw e;
+    }
+  });
 });
