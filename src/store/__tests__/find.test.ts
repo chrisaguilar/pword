56b793b6d4017a632911b61e6aed165d1238909e
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-16 22:54:26 -0500

    Changed the return value, follows conventions Also added test

--- src/store/__tests__/find.test.ts
+++ src/store/__tests__/find.test.ts
@@ -1,5 +1,6 @@
 import { destroy, initialize } from '.';
 import { close, find, open } from '..';
+import { b, green, r, red } from '../../lib/formatters';
 
 describe('store -> find', () => {
   beforeAll(async () => await initialize());
@@ -7,17 +8,31 @@ describe('store -> find', () => {
 
   test('finds passwords that match a given RegEx', async () => {
     try {
+
       let store = await open();
+
       store.set('Outlook MaIl', 'abc123');
       store.set('gmail', 'gmailPass');
       store.set('facebook', 'notmymailpass');
       await close(store);
 
-      const expected = [
-        ...store.entries()
-      ].filter(([ a, b ]) => /mail/i.test(a as string));
+      const expected =
+        [ ...store.entries() ]
+          .filter(([ a, b ]) => /mail/i.test(a as string))
+          .map(([ x, y ]) => `${b}${green}${x}${r}: ${y}`);
 
       expect(await find('mail')).toEqual(expected);
+
+    } catch (e) {
+      throw e;
+    }
+  });
+
+  test('alerts the user if no items match the RegEx', async () => {
+    try {
+
+      expect(await find('youtube')).toEqual(`${b}${red}No matches found for youtube${r}`);
+
     } catch (e) {
       throw e;
     }
--- src/store/find.ts
+++ src/store/find.ts
@@ -1,10 +1,18 @@
 import { open } from '.';
+import { b, green, r, red } from '../lib/formatters';
 
-export const find = async function find(term: string): Promise<Array<[{}, {}]>> {
+export const find = async function find(term: string): Promise<string|string[]> {
   try {
+
     const store: Map<{}, {}> = await open();
     const re: RegExp = new RegExp(term, 'i');
-    return [ ...store.entries() ].filter(([ a, b ]) => re.test(a as string));
+    const matches = [ ...store.entries() ].filter(([ a, b ]) => re.test(a as string));
+
+    if (matches.length === 0)
+      return `${b}${red}No matches found for ${term}${r}`;
+
+    return matches.map(([ x, y ]) => `${b}${green}${x}${r}: ${y}`);
+
   } catch (e) {
     throw e;
   }
