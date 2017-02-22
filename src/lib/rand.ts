216746e1bc3eb73c3440a925fdb6ac00ff3a708c
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-22 09:40:57 -0500

    I forgot that I wanted to change that to make more sense

--- src/lib/rand.ts
+++ src/lib/rand.ts
@@ -3,11 +3,8 @@ import { randomBytes } from 'crypto';
 // This is the function that generates the random hex bytes used throughout
 //   the program. It uses crypto.randomBytes to create random bytes of a
 //   given length.
-// Here, the specified length is divided by two (because when the bytes are
-//   converted to hex, they take up twice as many characters as needed),
-//   rounded down (to ensure that crypto.randomBytes receives an integer),
-//   and then passed through Math.abs, to make sure that crypto.randomBytes
-//   receives a positive number.
+// Here, the absolute value of the given length is divided by two, and then
+//   rounded down, in order to pass crypto.randomBytes a valid length.
 // The function returns a promise that resolves with the hex string, or
 //   rejects with the error, if any
 export const rand = function rand(length: number): Promise<string> {
