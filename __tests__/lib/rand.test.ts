b8942de2c9b7c9dd16c0fc422e4e2d9d3da57c75
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-22 09:37:54 -0500

    Added rand + test I'm doing a lot, today, mostly involving trying to get this to work properly.

---
+++ __tests__/lib/rand.test.ts
@@ -0,0 +1,25 @@
+import { rand } from 'lib/rand';
+
+describe('lib -> rand', () => {
+  test('returns a randomly-generated hex string of a given length', async () => {
+    try {
+      expect((await rand(10)).length).toBe(10);
+      expect((await rand(20)).length).toBe(20);
+      expect((await rand(-50)).length).toBe(50);
+      expect((await rand(12345)).length).toBe(12344);
+      expect((await rand(-98764)).length).toBe(98764);
+      expect((await rand(123.456)).length).toBe(122);
+      expect((await rand(-987.654)).length).toBe(986);
+    } catch (e) {
+      expect(e).toBeNull();
+    }
+  });
+  test('throws an error if given invalid input', async () => {
+    try {
+      await rand('hey' as any);
+    } catch (e) {
+      expect(e).not.toBeNull();
+      expect(e.name).toBe('TypeError');
+    }
+  });
+});
--- src/lib/rand.ts
+++ src/lib/rand.ts
@@ -1,10 +1,20 @@
 import { randomBytes } from 'crypto';
 
+// This is the function that generates the random hex bytes used throughout
+//   the program. It uses crypto.randomBytes to create random bytes of a
+//   given length.
+// Here, the specified length is divided by two (because when the bytes are
+//   converted to hex, they take up twice as many characters as needed),
+//   rounded down (to ensure that crypto.randomBytes receives an integer),
+//   and then passed through Math.abs, to make sure that crypto.randomBytes
+//   receives a positive number.
+// The function returns a promise that resolves with the hex string, or
+//   rejects with the error, if any
 export const rand = function rand(length: number): Promise<string> {
-  return new Promise<string>((resolve, reject) =>
-    randomBytes(
-      Math.floor(length / 2),
-      (err, buf) => err ? reject(err) : resolve(buf.toString('hex'))
-    )
-  );
+  const { abs, floor } = Math;
+  return new Promise<string>((resolve, reject) => {
+    return randomBytes(floor(abs(length) / 2), (err, buf) => {
+      return err ? reject(err) : resolve(buf.toString('hex'));
+    });
+  });
 };
