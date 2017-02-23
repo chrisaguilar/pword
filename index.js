95c1c0fca7a56570d6ca4e9ecaf297b18d4ae6d9
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-23 12:33:23 -0500

    This is correct.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 
-require('ts-node').register({
-  cacheDirectory: require('path').resolve(__dirname, '.tsnode_cache/'),
-  disableWarnings: true,
-  fast: true,
-  ignore: false,
-  lazy: true
-});
-require('./src/cli');
+(async () => {
+  try {
+    require('ts-node').register({
+      cacheDirectory: require('path').resolve(__dirname, '.tsnode_cache/'),
+      disableWarnings: true,
+      fast: true,
+      ignore: false,
+      lazy: true
+    });
+    require('./src/cli').exec();
+  } catch (e) {
+    return console.error(e);
+  }
+})();
+
