87d46f708b5a1da05900fd1279c0caa383d6efe7
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-23 13:02:00 -0500

    This is correct

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+import * as formatters from './formatters';
+import { init } from './init';
+import { rand } from './rand';
+import * as setup from './setup';
+
+export { formatters, init, rand, setup };
