cd6443b310268bd82727d7113439894e6f95ea08
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-22 19:48:43 -0500

    This is correct

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,31 +1,29 @@
 import { add } from './add';
-import { check } from './check';
 import { close } from './close';
 import { create } from './create';
 import { decrypt } from './decrypt';
 import { del } from './del';
 import { edit } from './edit';
 import { encrypt } from './encrypt';
+import { file } from './file';
 import { find } from './find';
 import { get } from './get';
-import { storefile } from './location';
 import { newpass } from './new';
 import { open } from './open';
 import { rename } from './rename';
 
 export {
   add,
-  check,
   close,
   create,
   decrypt,
   del,
   edit,
   encrypt,
+  file,
   find,
   get,
-  newpass,
+  newpass as new,
   open,
-  rename,
-  storefile
+  rename
 };
