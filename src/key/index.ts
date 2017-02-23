47aef4479ee9feb2b0f31e249d2b569b95ec7433
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-23 10:36:18 -0500

    Pretty much the final form of the key index This makes a hell of a lot more sense than what I had, before.

--- src/key/index.ts
+++ src/key/index.ts
@@ -1,18 +1,5 @@
-import { randomBytes } from 'crypto';
-import { check } from './check';
 import { create } from './create';
-import { keyfile } from './location';
+import { file } from './file';
 import { open } from './open';
 
-export { check, create, open, keyfile };
-
-export const key = async function key() {
-  try {
-
-    !await check() && await create();
-    return await open();
-
-  } catch (e) {
-    return console.error(e);
-  }
-};
+export { create, file, open };
