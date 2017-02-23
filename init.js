91b61a78a31aef4b257001aceaf5dc41b42c829c
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-23 10:22:39 -0500

    Added postinstall script This is going to be used to initialize the key + store, instead of *trying* to initialize it on every run of the program.

---
+++ init.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+(async () => {
+  try {
+    require('ts-node').register({
+      cacheDirectory: require('path').resolve(__dirname, '.tsnode_cache/'),
+      disableWarnings: true,
+      fast: true,
+      ignore: false,
+      lazy: true
+    });
+    await require('./src/lib/init').init();
+  } catch (e) {
+    return console.error(e);
+  }
+})();
