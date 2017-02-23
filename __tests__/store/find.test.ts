99acf1a728a5fba0833823b6ff92b40547072de5
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-22 19:47:15 -0500

    Find + test

---
+++ __tests__/store/find.test.ts
@@ -0,0 +1,51 @@
+import { remove } from 'fs-promise';
+
+import * as k from 'key';
+import { b, green, r, red } from 'lib/formatters';
+import { init } from 'lib/init';
+import * as s from 'store';
+
+describe('store -> find', () => {
+  beforeAll(async () => {
+    try {
+      await init();
+    } catch (e) {
+      return console.error(e);
+    }
+  });
+
+  afterAll(async () => {
+    try {
+      await remove(k.file);
+      await remove(s.file);
+    } catch (e) {
+      return console.error(e);
+    }
+  });
+
+  test('finds passwords that match a given RegEx', async () => {
+    try {
+      let store = await s.open();
+      store.set('Outlook MaIl', 'abc123');
+      store.set('gmail', 'gmailPass');
+      store.set('facebook', 'notmymailpass');
+      await s.close(store);
+      const expected =
+        [ ...store.entries() ]
+          .filter(([ a, b ]) => /mail/i.test(a as string))
+          .map(([ x, y ]) => `${b}${green}${x}${r}: ${y}`);
+      expect(await s.find('mail')).toEqual(expected);
+
+    } catch (e) {
+      expect(e).toBeNull();
+    }
+  });
+
+  test('alerts the user if no items match the RegEx', async () => {
+    try {
+      expect(await s.find('youtube')).toEqual(`${b}${red}No matches found for youtube${r}`);
+    } catch (e) {
+      expect(e).toBeNull();
+    }
+  });
+});
--- src/store/find.ts
+++ src/store/find.ts
@@ -1,19 +1,15 @@
-import { open } from '.';
-import { b, green, r, red } from '../lib/formatters';
+import { b, green, r, red } from 'lib/formatters';
+import * as s from 'store';
 
-export const find = async function find(term: string): Promise<string|string[]> {
+export const find = async function find(term: string) {
   try {
-
-    const store: Map<{}, {}> = await open();
+    const store = await s.open();
     const re: RegExp = new RegExp(term, 'i');
     const matches = [ ...store.entries() ].filter(([ a, b ]) => re.test(a as string));
-
     if (matches.length === 0)
       return `${b}${red}No matches found for ${term}${r}`;
-
     return matches.map(([ x, y ]) => `${b}${green}${x}${r}: ${y}`);
-
   } catch (e) {
-    throw e;
+    return console.error(e);
   }
 };
