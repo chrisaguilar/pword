2871b5c928914ee041041d13877bb7b5ffa8e9ae
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-23 13:02:25 -0500

    Init file for post-installation

---
+++ src/lib/init.ts
@@ -0,0 +1,26 @@
+import { exists, remove } from 'fs-promise';
+
+import * as k from '../key';
+import * as s from '../store';
+
+interface Init {
+  (): Promise<void>;
+}
+
+export const init: Init = async function() {
+  try {
+    if (!await exists(k.file) && !await exists(s.file)) {
+      await k.create();
+      await s.create();
+      return;
+    } else if (!await exists(k.file) || !await exists(s.file)) {
+      await remove(k.file);
+      await remove(s.file);
+      return await init();
+    } else {
+      return;
+    }
+  } catch (e) {
+    return console.error(e) as any;
+  }
+};
