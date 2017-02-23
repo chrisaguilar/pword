462ba03d9124efb9bd70cc1da3de297c19b13f20
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-22 19:47:04 -0500

    Edit + test

--- src/store/edit.ts
+++ src/store/edit.ts
@@ -1,19 +1,14 @@
-import { close, open } from '.';
-import { b, green, r, red } from '../lib/formatters';
+import { b, green, r, red } from 'lib/formatters';
+import * as s from 'store';
 
-export const edit = async function edit(name: string, password: string): Promise<string> {
+export const edit = async function edit(name: string, password: string) {
   try {
-
-    const store = await open();
-
+    const store = await s.open();
     if (!store.get(name)) return `${b}${red}${name} not in the store${r}`;
-
     store.set(name, password);
-    await close(store);
-
+    await s.close(store);
     return `${b}${green}${name}${r} set to ${b}${green}${password}${r}`;
-
   } catch (e) {
-    throw e;
+    return console.error(e);
   }
 };
