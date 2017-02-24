76619bb06ab939bea44d8a319502c1fe53dab0aa
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-02-24 10:42:39 -0500

    Fixes breaking changes from previous commit

--- src/key/__tests__/file.test.ts
+++ src/key/__tests__/file.test.ts
@@ -1,3 +1,4 @@
+import { homedir } from 'os';
 import { resolve } from 'path';
 
 import * as k from '..';
@@ -5,6 +6,6 @@ import * as k from '..';
 describe('key -> file', () => {
   test('should be a string', () => expect(typeof k.file).toBe('string'));
 
-  const path = resolve(__dirname, '../../../key');
+  const path = resolve(homedir(), '.pword/key');
   test('should point to the correct location', () => expect(path).toEqual(k.file));
 });
